523a8d68ba5fd0165c5ee0406933fabe90340c74
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-26 23:01:27 +0530

    Added: Hotel name and rate with the Junit Testcase

---
+++ src/main/java/com/bl/java/hotel_reservation/HotelReservationSystem.java
@@ -0,0 +1,23 @@
+package com.bl.java.hotel_reservation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservationSystem {
+
+    private List<Hotel> hotels;
+
+    public HotelReservationSystem() {
+        this.hotels = new ArrayList<>();
+    }
+
+    public void addHotel(String name, double rate) {
+        Hotel hotel = new Hotel(name,rate);
+        hotels.add(hotel);
+    }
+
+    public List<Hotel> getHotel() {
+        return hotels;
+    }
+}
+
+
