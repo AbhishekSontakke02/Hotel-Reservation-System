c02eb1182b32c034ef1342c9084567477cb53429
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-26 23:43:20 +0530

    added: function to find cheapest best rated hotel and exception handling to check invalid input

---
+++ src/main/java/com/bl/java/hotel_reservation/InvalidInputException.java
@@ -0,0 +1,10 @@
+package com.bl.java.hotel_reservation;
+
+public class InvalidInputException extends Exception {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
+
+
